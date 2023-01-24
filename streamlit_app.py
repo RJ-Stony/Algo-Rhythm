745f844311fc06199a0df97712ca28a9f07e7c6c
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-24 14:31:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,15 @@
 import streamlit as st
 import time
 
-with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
 
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
