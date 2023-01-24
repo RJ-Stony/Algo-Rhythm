8619a5e2dbda175dfad085be1f4dd530211cc730
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-24 14:48:53 +0900

    Update streamlit_app.py
    Add category for Shortest Path

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,21 @@ st.sidebar.header('Algo List')
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     'Which algorithm do you want?',
-    ('DP, Dynamic Programming', 'Greedy', 'Brute Forcing', 'Sorting',
-     'Binary Search', 'BFS, Breadth First Search', 'DFS, Depth First Search',
-     'Floyd Warshall')
+    ('DP, Dynamic Programming', 'Greedy', 'Brute Forcing', 'Sorting', 'Shortest Path',
+     'Binary Search', 'BFS, Breadth First Search', 'DFS, Depth First Search')
 )
 
-if add_selectbox == 'Floyd Warshall':
-    st.write('This is Floyd Warshall')
+if add_selectbox == 'Shortest Path':
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.header('DIJKSTRA')
+        st.button('This is DIJKSTRA')
+
+    with col2:
+        st.header('Floyd Warshall')
+        st.button('This is Floyd Warshall')
+
+    with col3:
+        st.header('Bellman Ford')
+        st.button('This is Bellman Ford')
