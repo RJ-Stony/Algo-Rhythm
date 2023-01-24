d1968413c17eab83f12e74d4321961593d230b01
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-24 14:38:37 +0900

    Update streamlit_app.py
    Add title and sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,15 @@
 import streamlit as st
 import time
 
+st.title('Algo-Rhythm 🎼')
+st.sidebar.header('Algo List')
+
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     'Which algorithm do you want?',
     ('DP, Dynamic Programming', 'Greedy', 'Brute Forcing', 'Sorting',
-     'Binary Search', 'BFS, Breadth First Search', 'DFS, Depth First Search')
+     'Binary Search', 'BFS, Breadth First Search', 'DFS, Depth First Search',
+     'Floyd Warshall')
 )
+
+
